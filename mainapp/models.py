595774b53781e2903ace926c883c71f6d049015d
Sergey <so.bunin@gmail.com>
2022-10-28 19:54:54 +0300

    задание к уроку 4

---
+++ mainapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    preamble = models.CharField(max_length=1024, verbose_name='Вступление')
+
+    body = models.TextField(verbose_name='Содержимое')
+    body_as_markdown = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
+    deleted_at = models.BooleanField(default=False, verbose_name='Удалено')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
